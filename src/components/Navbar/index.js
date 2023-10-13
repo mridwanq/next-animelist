a721ea826c74be24b9e5d609cdc03c0e1611a71a
Moch Ridwan Qodri <ridwanqodryy@gmail.com>
2023-10-13 08:06:09 +0700

    up

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,9 +4,9 @@ import InputSearch from './InputSearch';
 const Navbar = () => {
   return (
     <header className='bg-yellow-400'>
-      <div className='flex md:flex-row flex-col justify-between p-4 gap-2'>
+      <div className='flex md:flex-row flex-col justify-between p-4 gap-'>
         <Link href='/' className='font-bold text-black text-2xl'>
-          ANIME LIST
+          ANIME LIST<span className='animate-pulse'>🚀</span>
         </Link>
         <InputSearch />
       </div>
